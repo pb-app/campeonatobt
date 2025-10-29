5e4be61928b41d6557984865bfa3d2d27b5f045e
pb-app <uegafs@gmail.com>
2025-10-29 15:16:16 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
 // ATENÇÃO: Mude a versão do cache (ex: v4.0.8) sempre que fizer deploy de novos arquivos
-const CACHE_NAME = 'ranking-bt-v4.0.6';
+const CACHE_NAME = 'ranking-bt-v4.0.9';
 
 // Arquivos principais do app (App Shell)
 // ADICIONEI 'index.html' e atualizei para 'teste2.html' (que você usa)
